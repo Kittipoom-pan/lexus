159170ece7425b97ee951c32774ac350b38c7d20
kittipoom.p <kittipoom@feyverly.com>
2021-06-15 21:15:39 +0700

    tricker noti

---
+++ TrickerNotification/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrickerNotification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string URL = ConfigurationManager.AppSettings["url"].ToLower();
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create(URL);
+            httpWebRequest.Method = "GET";
+
+            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            string response;
+
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                response = streamReader.ReadToEnd();
+            }
+        }
+    }
+}
